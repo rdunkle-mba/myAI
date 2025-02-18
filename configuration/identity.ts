12e50a315a25641efaf6835e71765ecabbc82136
rdunkle-mba <ryandun1990@gmail.com>
2025-02-18 12:13:18 -0500

    Update identity.ts
    Updated OWNER_DESCRIPTION

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `Ryan`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_DESCRIPTION: string = `A management consultant leveraging AI to automate tasks and develop innovative solutions for the world's hardest problems.`;
 
 export const AI_NAME: string = `[AI NAME]`;
 export const AI_TONE: string = `[AI TONE]`;
