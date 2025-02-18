c9056246153a80bc891a15907d9d1d97dffa07d7
rdunkle-mba <ryandun1990@gmail.com>
2025-02-18 17:46:30 -0500

    Update ui.ts
    Updated FOOTER_MESSAGE

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -2,7 +2,7 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `YenAI: Smarter Than You, Faster Than You, Here to Help Anyway.`;
 export const MESSAGE_PLACEHOLDER: string = `Ask wisely, and I may grace you with an answer…`;
-export const FOOTER_MESSAGE: string = `Management Consultant Assistant for Ryan Dunkle`;
+export const FOOTER_MESSAGE: string = `Strategy is power. Information is control. And I have both.`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
 export const PAGE_TITLE: string = `[PAGE TITLE]`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
