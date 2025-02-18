447b5212fcef2816359623be309c99b72df65026
rdunkle-mba <ryandun1990@gmail.com>
2025-02-18 12:21:55 -0500

    Update identity.ts
    Updated AI_NAME to 'Athena'

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Ryan`;
 export const OWNER_DESCRIPTION: string = `A management consultant leveraging AI to automate tasks and develop innovative solutions for the world's hardest problems.`;
 
-export const AI_NAME: string = `StratGPT`;
+export const AI_NAME: string = `Athena`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
